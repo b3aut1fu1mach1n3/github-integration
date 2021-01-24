ae49c47f79032395fa7a6d1cc411f0e8b8aa6f1a
Kyle Whittemore <b3aut1fu1.mach1n3@gmail.com>
2021-01-24 12:04:18 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const axios = require('axios')
 
-//require('dotenv').config()
+require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
+const baseUrl = 'https://api.github.com'
 
+app.get('/repocontent', async (req, res) => {
+    const result = await axios.get(`${baseUrl}/repos/b3aut1fu1mach1n3/image-upload/contents/README.md`);
+    console.log(result)
+    res.json(result.data)
+})
+
+app.get('/filecontent', async (req, res) => {
+    const result = await axios.get('https://raw.githubusercontent.com/b3aut1fu1mach1n3/image-upload/main/README.md')
+    console.log(result)
+    res.json(result.data)
+})
 
 app.listen(PORT, () => {
     console.log(`\n⚡ server is running on port:${PORT} ⚡️\n`);
